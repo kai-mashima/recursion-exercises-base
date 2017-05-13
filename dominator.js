0b376ac3d6ad68c75d1fe3ce94d70f489c5d05cf
Kai Mashima <kaiomashima@gmail.com>
2017-05-12 21:29:26 -0700

    all tests passed

--- dominator.js
+++ dominator.js
@@ -2,15 +2,38 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+  const tree = flattenTreeToArray(root);
+  for (let i = 0; i < tree.length; i++) {
+    if(tree[i].id === id) {
+      return tree[i]
+    }
+  }
 };
 
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  const tree = flattenTreeToArray(root);
+  const classArr = [];
+  for (let i = 0; i < tree.length; i++) {
+    const classNames= tree[i].className;
+    if (classNames !== undefined) {
+      const classNameIndex = classNames.indexOf(className);
+      if(classNameIndex !== -1) {
+        classArr.push(tree[i]);
+      }
+    } 
+  }
+  return classArr;
 };
 
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  const tree = flattenTreeToArray(root);
+  const tagNameArr = [];
+  for (let i = 0; i < tree.length; i++) {
+    if(tree[i].tagName === tagName) {
+      tagNameArr.push(tree[i]);
+    }
+  }
+  return tagNameArr;
 };
 
 module.exports = {
--- jsoninator.js
+++ jsoninator.js
@@ -1,14 +1,55 @@
-const _ = require('underscore'); // the real one! :)
-
-// This is what you would do if you liked things to be easy:
-// const stringify = JSON.stringify;
-// But you don't. So you're going to write it from scratch...
-
-
+const _ = require('underscore'); 
 
 const stringify = function(obj) {
-  // your code goes here
-};
+
+  function isScalar(obj) {
+    return (/string|number|boolean/).test(typeof obj);
+  }
+
+  const printArray = function(obj) {
+    if(obj.length === 0) {
+      return "[]";
+    } else {
+      const arr = [];
+      _.each(obj, (el) => arr.push(stringify(el)));
+      return '[' + arr + ']';
+    }
+  }
+
+  const printObj = function(obj) {
+    //console.log(obj);
+    if(Object.keys(obj).length === 0) {
+      return "{}";
+    } else {
+      for(let key in obj) {
+        if(isScalar(key) && isScalar(obj[key])) {
+          return '{"' + key + '":"' + obj[key] + '"}';
+        } else if (!isScalar(key) && isScalar(obj[key])) {
+          return '{' + stringify(key) + ':"' + obj[key] + '"}';
+        } else if (isScalar(key) && !isScalar(obj[key])) {
+          return '{"' + key + '":' + stringify(obj[key]) + '}';
+        } else if (!isScalar(key) && !isScalar(obj[key])) {
+          return '{' + stringify(key) + ':' + stringify(obj[key]) + '}';
+        }
+      }
+    }
+  }
+
+  const objType = typeof obj;
+  
+  if(objType === 'string') { //DONE
+    return "\"" + obj + "\"";
+  } else if (obj === null) { //DONE
+    return 'null';
+  } else if(objType === 'number' || objType === 'boolean') { //DONE
+    return String(obj); 
+  } else if(Array.isArray(obj)) { //broken
+    return printArray(obj);
+  } else if(objType === 'object') { //returns object not in string
+    return printObj(obj);;
+  }
+
+}
 
 module.exports = {
   stringify: stringify
